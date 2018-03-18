1c4e82c525756f131bab1d5190fa581d5d0fd940
romulusbita <romulus.bita@nttdata.ro>
2018-03-19 01:10:41 +0200

    Added software requirements IDs

--- src/INIT/system_init.c
+++ src/INIT/system_init.c
@@ -18,13 +18,14 @@ void Init_system(void)
 
     SSPInit();
 
+    // SW-COMM-ROT-0001(1)
     rotary_init();
     oled_init();
 
     oled_clearScreen(OLED_COLOR_WHITE);
 
     // by default, init in rotary state
-    oled_putString(1,1,  (uint8_t*)"Rotary Value:", OLED_COLOR_BLACK, OLED_COLOR_WHITE);
+    oled_putString(1,1,  (uint8_t*)"Rotary Value:0", OLED_COLOR_BLACK, OLED_COLOR_WHITE); // SW-COMM-ROT-0009(1)
 
     // button init
     Btn_mapping();
--- src/ROT/rotary_module.c
+++ src/ROT/rotary_module.c
@@ -9,7 +9,7 @@
 #include "../BTN/buttons_module.h"
 
 static uint8_t Rot_encoderState    =  0;
-static uint8_t Rot_encoderCnt	   = '0';
+static uint8_t Rot_encoderCnt	   = '0'; // SW-COMM-ROT-0006(1)
 uint8_t Btn_resetEncoderCnt 	   =  0;
 
 void Rot_modeSelected(void)
@@ -17,23 +17,29 @@ void Rot_modeSelected(void)
 	Rot_encoderState = rotary_read();
 	Btn_resetEncoderCnt = GPIOGetValue(PORT0, 1);
 
+	// SW-COMM-ROT-0008(1)
 	if (Btn_resetEncoderCnt == 0)
 	{
 		Rot_encoderCnt = '0';
 		Rot_printValue(Rot_encoderCnt);
 
-	}
+	} // SW-COMM-ROT-0002(1)
 	else if (Rot_encoderState != ROTARY_WAIT)
 	{
+		// SW-COMM-ROT-0002(1)
 	    if (Rot_encoderState == ROTARY_RIGHT)
 	    {
+	    	// SW-COMM-ROT-0003(1)
 	    	Rot_encoderCnt++;
 	    }
-	    else
+	    // SW-COMM-ROT-0002(1)
+	    else // CCW
 	    {
+	    	// SW-COMM-ROT-0004(1)
 	    	Rot_encoderCnt--;
 	    }
 
+	    // SW-COMM-ROT-0011(1)
 	    if (Rot_encoderCnt > '9')
 	    {
 	    	Rot_encoderCnt = '0';
@@ -43,7 +49,13 @@ void Rot_modeSelected(void)
 	    	Rot_encoderCnt = '9';
 	    }
 
+	    // SW-COMM-ROT-0007(1)
 	    Rot_printValue(Rot_encoderCnt);
+	}
+	else
+	{
+		// SW-COMM-ROT-0005(1)
+		// The position counter shall not alter its value, if the rotary encoder is not changed.
 	}
 }
 
@@ -51,7 +63,10 @@ void Rot_modeSelected(void)
 void Rot_printValue(uint8_t p_encoderCnt)
 {
 	/* output values to OLED display */
-	Oled_intToString((p_encoderCnt-48), buf, 10, 10);
+	// SW-COMM-ROT-0003(1)
+	// SW-COMM-ROT-0004(1)
+	// SW-COMM-ROT-0010(1)
+	Oled_intToString(((p_encoderCnt-48)*10), buf, 10, 10);
 	oled_fillRect((1+13*6),1, 80, 8, OLED_COLOR_WHITE);
 	oled_putString(1+13*6,1, buf, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
 }
